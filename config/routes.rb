09ed37f84e329f473cc47f4e023712288b592a6b
Ashvith Shetty <ashvithvittal@gmail.com>
2020-12-15 21:51:58 +0530

    This is the first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # Home page
+  root 'home#index'
+  #About page
+  get 'about', to: 'home#about', as: 'about'
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
