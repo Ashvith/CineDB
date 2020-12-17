4f533876b46cb19fb15b8cfcc909daf4b995fd6d
Ashvith Shetty <ashvithvittal@gmail.com>
2020-12-17 16:34:08 +0530

    Database demo insertion

---
+++ app/models/movie.rb
@@ -0,0 +1,8 @@
+class Movie
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  field :movie_id, type: String
+  field :title, type: String
+  field :cast, type: String
+  field :crew, type: String
+end
